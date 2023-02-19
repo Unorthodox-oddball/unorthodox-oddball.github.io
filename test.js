0eaab67da900e9b9bcb4850c7e3363e7ac1b09fb
Unorthodox-oddball <100816088+Unorthodox-oddball@users.noreply.github.com>
2023-02-19 21:35:30 +0900

    Update test.js

--- test.js
+++ test.js
@@ -30,19 +30,3 @@ document.addEventListener('click', function() {
   audio.loop = true;
   document.removeEventListener('click', arguments.callee);
 });
-
-// 저작권 정보 추가
-const currentYear = new Date().getFullYear();
-const startYear = 2023;
-let copyrightText = "";
-
-if (currentYear === startYear) {
-  copyrightText = startYear;
-} else {
-  copyrightText = startYear + " - " + currentYear;
-}
-
-document.querySelector(".copyright-year").textContent = copyrightText;
-
-const lastUpdated = new Date().toLocaleDateString();
-document.querySelector(".last-updated").textContent = lastUpdated;
