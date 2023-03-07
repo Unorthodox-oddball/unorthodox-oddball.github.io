4136b43cafe13dacfe4cb71b53600d6b96fc9743
Unorthodox-oddball <100816088+Unorthodox-oddball@users.noreply.github.com>
2023-03-07 21:20:50 +0900

    Update test.js

--- test.js
+++ test.js
@@ -42,3 +42,43 @@ document.addEventListener('keydown', function(event) {
     myWindow.focus();
   }
 
+function openModal(pdfUrl) {
+  // Get the modal and the close button
+  var modal = document.getElementById("modal");
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on the button, open the modal
+  modal.style.display = "block";
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
+  // Load the PDF into the PDF.js viewer
+  var pdfjsLib = window['pdfjs-dist/build/pdf'];
+  pdfjsLib.GlobalWorkerOptions.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';
+  var pdfContainer = document.getElementById("pdf-container");
+
+  pdfjsLib.getDocument(pdfUrl).promise.then(function(pdfDoc) {
+    for (var i = 1; i <= pdfDoc.numPages; i++) {
+      pdfDoc.getPage(i).then(function(page) {
+        var canvas = document.createElement("canvas");
+        canvas.className = "pdf-page";
+        pdfContainer.appendChild(canvas);
+
+        var viewport = page.getViewport({scale: 1.5});
+        var context = canvas.getContext("2d");
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+
+        var renderContext = {
+          canvasContext: context
+
